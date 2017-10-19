9326ec44374bee04d10354b2ed9c2a99d1537c7e
Lawerence Lee <lucifitz.edward@gmail.com>
2017-10-19 00:30:36 -0700

    added date_format validator to date in AddEntryForm

--- forms.py
+++ forms.py
@@ -1,10 +1,13 @@
+from datetime import datetime
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 
-def date_format():
-    pass
+def date_format(form, field):
+    if not datetime.strptime(field.data, '%m/%d/%Y'):
+        raise ValidationError('Date format is NOT correct.')
 
 
 class AddEntryForm(FlaskForm):
@@ -17,12 +20,13 @@ class AddEntryForm(FlaskForm):
         'Date (MM/DD/YYYY):',
         validators=[
             DataRequired(),
-            # date_format
+            date_format,
         ])
     time_spent = StringField(
         'Time Spent (MINS):',
         validators=[
             DataRequired()
+            # int_checker
         ])
     learned = TextAreaField(
         "What I Learned:",
