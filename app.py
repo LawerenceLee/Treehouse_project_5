3bbb917428d25c29150a88a6eef9209a93d83212
Lawerence Lee <lucifitz.edward@gmail.com>
2017-10-21 20:28:28 -0700

    Add slugify function, and change routes to use slugs

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import (Flask, render_template, url_for, g,
                    flash, redirect)
 
@@ -11,6 +13,12 @@ PORT = 8000
 
 app = Flask(__name__)
 app.secret_key = 'jri,74gdk20jsynGRYm387*(^$&jfhbfd6GR$fv$%YGR^*HD33RGe4fR'
+
+
+def slugify(string):
+    string = re.sub(r'[_.\s]', '-', string).lower()
+    string = re.sub(r'\\', '', string)
+    return re.sub('[?#$%^&!@*]', '', string)
 
 
 @app.before_request
@@ -30,10 +38,10 @@ def after_request(response):
 
 # Needs two routes
 @app.route('/entry', methods=['GET', 'POST'])
-@app.route('/entries/edit/<edit_id>', methods=['GET', 'POST'])
-def add_edit(edit_id=None):
-    if edit_id:
-        entry = models.Entry.get(models.Entry.id == edit_id)
+@app.route('/entries/edit/<slug>', methods=['GET', 'POST'])
+def add_edit(slug=None):
+    if slug:
+        entry = models.Entry.get(models.Entry.slug == slug)
         form = forms.AddEditEntryForm()
         if form.validate_on_submit():
             models.Entry.update(
@@ -41,8 +49,9 @@ def add_edit(edit_id=None):
                 date=form.date.data.strip(),
                 time_spent=form.time_spent.data.strip(),
                 learned=form.learned.data.strip(),
-                resources=form.resources.data.strip()
-                ).where(models.Entry.id == edit_id
+                resources=form.resources.data.strip(),
+                slug=slugify(form.title.data.strip())
+                ).where(models.Entry.slug == slug
                         ).execute()
             flash("Entry Updated!", 'success')
             return redirect(url_for('index'))
@@ -60,27 +69,28 @@ def add_edit(edit_id=None):
                 date=form.date.data.strip(),
                 time_spent=form.time_spent.data,
                 learned=form.learned.data.strip(),
-                resources=form.resources.data.strip())
+                resources=form.resources.data.strip(),
+                slug=slugify(form.title.data.strip()))
             flash("Entry Added!", 'success')
             return redirect(url_for('index'))
         return render_template('new.html', form=form)
 
 
-@app.route('/entries/<entry_id>')
-def detail(entry_id):
-    entry = models.Entry.get(models.Entry.id == entry_id)
+@app.route('/entries/<slug>')
+def detail(slug=None):
+    entry = models.Entry.get(models.Entry.slug == slug)
     return render_template('detail.html', entry=entry)
 
 
-@app.route('/entries/delete/<entry_id>')
-def delete(entry_id=None):
-    models.Entry.get(models.Entry.id == entry_id).delete_instance()
+@app.route('/entries/delete/<slug>')
+def delete(slug=None):
+    models.Entry.get(models.Entry.slug == slug).delete_instance()
     return redirect(url_for('index'))
 
 
 @app.route('/')
 @app.route('/entries')
-def index(entry_id=None):
+def index():
     entries = models.Entry.select().limit(100)
     return render_template('index.html', entries=entries)
 
