cdc0db5cf3b542e955402e55fd03b2a368d0ea3f
Lawerence Lee <lucifitz.edward@gmail.com>
2017-10-22 02:06:12 -0700

    added user class, fixed imports, added tags to entry class, added docstrings

--- models.py
+++ models.py
@@ -1,6 +1,26 @@
+from flask_bcrypt import generate_password_hash
+from flask_login import UserMixin
 from peewee import *
 
 DATABASE = SqliteDatabase('entries.db', threadlocals=True)
+
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    password = CharField(max_length=100)
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_user(cls, username, password):
+        try:
+            with DATABASE.transaction():
+                cls.create(
+                    username=username,
+                    password=generate_password_hash(password))
+        except IntegrityError:
+            raise ValueError("User already exists")
 
 
 class Entry(Model):
@@ -10,12 +30,15 @@ class Entry(Model):
     learned = TextField()
     resources = TextField()
     slug = CharField(max_length=100)
+    tags = CharField(max_length=200)
 
     class Meta:
         database = DATABASE
 
 
 def initialize():
+    '''Open database, create tables if they are not already
+    there, close database'''
     DATABASE.connect()
-    DATABASE.create_tables([Entry], safe=True)
+    DATABASE.create_tables([Entry, User], safe=True)
     DATABASE.close()
